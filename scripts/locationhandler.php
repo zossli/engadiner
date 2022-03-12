4266795205ce736b0b6f27207afe1d590c646ea9
Reto <zossli@users.noreply.github.com>
2022-03-12 11:32:23 +0100

    Added 1 hour... Gruuusig

--- scripts/locationhandler.php
+++ scripts/locationhandler.php
@@ -12,7 +12,7 @@ $line = "<competitors>";
 
     while ($obj = $rst->fetch_object())
     {
-        $time = $obj->time;
+        $time = $obj->time+3600;
         $line.="<competitor><name>";
         $line.=$obj->name;
         $line.="</name> <unique>";
